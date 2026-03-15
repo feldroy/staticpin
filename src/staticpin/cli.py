756ff59ac6056817ae5e5ef501734e8faff597e8
Daniel Roy Greenfeld <daniel@feldroy.com>
2026-03-15 15:27:27 +0800

    Annotate the verson argument

--- src/staticpin/cli.py
+++ src/staticpin/cli.py
@@ -1,28 +1,37 @@
 """Console script for staticpin."""
 
+from pathlib import Path
+
 import typer
 from rich.console import Console
-
-from staticpin import utils
 
 app = typer.Typer()
 console = Console()
 
 
+@app.command()
+def add(
+    name: str,
+    version: str = typer.Argument("", help="Version to install. If not specified, the latest version will be used."),
+    static_dir: Path = Path("static"),
+):
+    """Add a dependency."""
+    console.print(f"Adding {name}")
+
 
 @app.command()
-def add(dep: str, version: str = ''):
-    """Add a dependency, if no version is specified pull the latest one"""
+def upgrade(
+    name: str,
+    version: str = typer.Argument("", help="Version to upgrade to. If not specified, the latest version will be used."),
+    static_dir: Path = Path("static"),
+):
+    """Update a dependency."""
     pass
 
-@app.command()
-def upgrade(dep: str, version: str = ''):
-    """Update a dependency, if no version is specified update to the latest one"""
-    pass
 
 @app.command()
-def remove(dep: str):
-    """Remove a dependency, if no version is specified pull the latest one"""
+def remove(name: str, static_dir: Path = Path("static")):
+    """Remove a dependency."""
     pass
 
 
--- src/staticpin/utils.py
+++ src/staticpin/utils.py
@@ -1,2 +1,16 @@
-def do_something_useful() -> None:
-    print("Replace this with a utility function")
+import pathlib
+
+import httpx
+
+
+def fetch_by_version(name: str, version: str, static_dir: pathlib.Path):
+    # Using cdnjs API
+    url = f"https://api.cdnjs.com/libraries/{name}/{version}"
+    meta = httpx.get(url).json()
+    for file in meta["files"]:
+        if file.endswith(".min.js") or file.endswith(".min.css"):
+            content = httpx.get(f"https://cdnjs.cloudflare.com/ajax/libs/{name}/{version}/{file}")
+            dest = static_dir / name / file
+            dest.parent.mkdir(parents=True, exist_ok=True)
+            dest.write_bytes(content.content)
+            print(f"✓ {dest}")
